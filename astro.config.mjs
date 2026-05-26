ff0e37f26d5802c42870c86b24f3af85457c6f9c
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2026-05-26 08:46:08 +0200

    chore: add github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://lucabruessow.github.io',
+    base: '/tattoo-website'
+});
