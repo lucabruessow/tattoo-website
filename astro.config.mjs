ad237a108f44a7c239af071b8696a7d993f32c52
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2026-05-26 22:24:26 +0200

    add self-hosted Inter and Mrs Sheppards fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lucabruessow.github.io',
-    base: '/tattoo-website'
+    base: '/tattoo-website',
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "Inter",
+            cssVariable: "--font-inter",
+            options: {
+                variants: [
+                    {
+                        weight: "100 900",
+                        style: "normal",
+                        src: ["./src/assets/fonts/Inter-Regular.woff2"],
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Mrs Sheppards",
+            cssVariable: "--font-mrs-sheppard",
+            options: {
+                variants: [
+                    {
+                        weight: "400",
+                        style: "normal",
+                        src: ["./src/assets/fonts/MrsSheppards-Regular.woff2"]
+                    }
+                ]
+            }
+
+        }
+    ]
 });
